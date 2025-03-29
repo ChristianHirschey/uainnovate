d6fb921835a52ccdd23f4915f5b95f3f68d66566
Christian Hirschey <christian.hirschey@gmail.com>
2025-03-29 16:27:36 -0500

    CRUD for notifications & notification model

--- backend/app/models/notification.py
+++ backend/app/models/notification.py
@@ -1,1 +1,23 @@
-#
+# app/models/notification.py
+from pydantic import BaseModel
+from typing import Optional
+from uuid import UUID
+
+class NotificationBase(BaseModel):
+    message: str
+    supply_id: Optional[int] = None
+    request_id: Optional[int] = None
+    seen: Optional[bool] = False
+
+class NotificationCreate(NotificationBase):
+    pass
+
+class NotificationRead(NotificationBase):
+    id: UUID
+
+class NotificationUpdate(NotificationBase):
+    id: UUID
+
+class NotificationDelete(NotificationBase):
+    id: UUID
+    
