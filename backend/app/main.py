3fbc12f1a7d900eefc123411b74d95184259b622
Gavin Ward <gbward@crimson.ua.edu>
2025-03-29 15:08:07 -0500

    main route

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,9 +5,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.supabase.supabaseClient import supabase
 from app.models.supply import Supply
 from app.utils.supply_ops import create_supply_record
-
+from routes import logs, notifications, supplies, requests
 app = FastAPI()
-
+app.include_router(logs.router, prefix="/api", tags=["logs"])
+app.include_router(notifications.router, prefix="/api", tags=["notifications"])
+app.include_router(supplies.router, prefix="/api", tags=["supplies"])
+app.include_router(requests.router, prefix="/api", tags=["requests"])
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,
