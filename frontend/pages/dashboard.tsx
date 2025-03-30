636a7da7fe604768ad2da61aa42a72069240f0d3
jroy <jayyk0912@gmail.com>
2025-03-30 09:06:54 -0500

    adding frontend

---
+++ frontend/pages/dashboard.tsx
@@ -0,0 +1,7 @@
+"use client"
+import '../app/globals.css';
+
+import { DashboardLayout } from "@/components/dashboard-layout"
+export default function Dashboard() {
+    return <DashboardLayout />
+}
